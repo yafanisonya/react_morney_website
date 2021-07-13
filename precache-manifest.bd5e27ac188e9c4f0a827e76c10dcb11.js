e3391cb0e292def0891a846c194c3a8ee80926d0
fanison <308955874@qq.com>
2021-07-13 23:09:41 +0800

    init

---
+++ precache-manifest.bd5e27ac188e9c4f0a827e76c10dcb11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420b863ab32f11712b02ee365d446780",
+    "url": "/react_morney_website/index.html"
+  },
+  {
+    "revision": "f0bd7189562de7b45824",
+    "url": "/react_morney_website/static/css/main.3d9c248f.chunk.css"
+  },
+  {
+    "revision": "c95c042b822f9a448ce6",
+    "url": "/react_morney_website/static/js/2.9ef2d9c6.chunk.js"
+  },
+  {
+    "revision": "fc8b115d4064d6797b6def218d457695",
+    "url": "/react_morney_website/static/js/2.9ef2d9c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0bd7189562de7b45824",
+    "url": "/react_morney_website/static/js/main.0c42cdc8.chunk.js"
+  },
+  {
+    "revision": "b0a58cda1bf574b151eb",
+    "url": "/react_morney_website/static/js/runtime-main.d845c808.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react_morney_website/precache-manifest.bd5e27ac188e9c4f0a827e76c10dcb11.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_morney_website/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
